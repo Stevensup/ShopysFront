e41a28f4a621d616679b119875d2d6766091eff6
Stevensup <buseche@unbosque.edu.co>
2023-11-12 15:41:13 -0500

    ajuste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,19 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { ConsultaProductosComponent } from './consulta-productos/consulta-productos.component';
 import { VentasComponent } from './ventas/ventas.component';
 import { FacturacionComponent } from './facturacion/facturacion.component';
-import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/productos', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'productos', component: ConsultaProductosComponent },
   { path: 'ventas', component: VentasComponent },
   { path: 'facturacion', component: FacturacionComponent },
-  { path: 'login', component: LoginComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CarritoService } from '../carrito.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,12 +10,33 @@ import { CarritoService } from '../carrito.service';
 export class NavbarComponent {
   cantidadEnCarrito: number = 0;
 
-  constructor(private carritoService: CarritoService) {}
+  constructor(private carritoService: CarritoService, private router: Router) {}
+
 
   ngOnInit(): void {
-    // Suscribirse al BehaviorSubject para obtener actualizaciones de la cantidad en el carrito
     this.carritoService.obtenerCantidadEnCarrito().subscribe(cantidad => {
       this.cantidadEnCarrito = cantidad;
     });
   }
+
+  // Método para navegar a la página de productos
+  navegarAProductos() {
+    this.router.navigate(['/productos']);
+  }
+
+  // Método para navegar a la página de ventas
+  navegarAVentas() {
+    this.router.navigate(['/ventas']);
+  }
+
+  // Método para navegar a la página de facturación
+  navegarAFacturacion() {
+    this.router.navigate(['/facturacion']);
+  }
+
+  navegarALogin() {
+    this.router.navigate(['/login']);
+  }
+
+  // Otros métodos de navegación según sea necesario
 }
